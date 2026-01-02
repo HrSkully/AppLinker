e510916b366622acd40e3c012bab4ec5cc772d3f
HrSkully <timo.rosen@outlook.com>
2026-01-02 20:41:53 +0100

    Lizenz hinzugefügt (MIT)

--- core/main.py
+++ core/main.py
@@ -1,16 +1,14 @@
 import sys
 import os
 from PyQt6.QtWidgets import QApplication, QFileDialog, QMessageBox
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from ui.maingui import YAILGui
 from logic import create_desktop_file
 
 class MainWindow(YAILGui):
     def __init__(self):
         super().__init__()
-        # Signale verbinden
+
         self.btn_browse_app.clicked.connect(self.select_app)
         self.btn_browse_icon.clicked.connect(self.select_icon)
         self.btn_create.clicked.connect(self.run_logic)
@@ -24,11 +22,19 @@ class MainWindow(YAILGui):
         if file: self.icon_input.setText(file)
 
     def run_logic(self):
+        self.btn_create.setEnabled(False)
+        self.btn_create.setText("Warte auf Autorisierung...")
+        QApplication.processEvents()
+
         success, msg = create_desktop_file(
             self.name_input.text(),
             self.path_input.text(),
             self.icon_input.text()
         )
+
+        self.btn_create.setEnabled(True)
+        self.btn_create.setText("Systemweit registrieren")
+
         if success:
             QMessageBox.information(self, "Erfolg", f"Erstellt: {msg}")
         else:
